ee0185373652e06226030a49770e0fc6622086ee
arfani <arfanihidayat@gmail.com>
2023-12-04 13:43:23 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { domains: ['media.giphy.com', 'localhost', 'api-percepat.bbpommataram.id'] }
+    images: { domains: ['media.giphy.com', 'localhost', 'api-percepat.bbpommataram.id'] },
+    experimental:{
+        appDir:true
+    }
 }
 
 module.exports = nextConfig
