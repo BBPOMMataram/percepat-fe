6f7e7a68d30e9c35653879ab2e61809ba8f12fbd
arfani <arfanihidayat@gmail.com>
2024-05-21 15:07:54 +0800

    update url image next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { domains: ['media.giphy.com', 'localhost', 'api-percepat.bbpommataram.id'] }
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+            },
+            {
+                protocol: 'https',
+                hostname: 'percepat-api.bbpommataram.id',
+            },
+            {
+                protocol: 'https',
+                hostname: 'media.giphy.com',
+            },
+        ],
+    }
 }
 
 module.exports = nextConfig
