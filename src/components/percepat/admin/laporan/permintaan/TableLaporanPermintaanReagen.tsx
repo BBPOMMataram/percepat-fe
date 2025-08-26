a613a2d27219ef311813dc45a08015ac6ecd1e60
arfani <arfanihidayat@gmail.com>
2025-08-26 11:18:14 +0800

    update filter tahun dropdown di laporan permintaan

--- src/components/percepat/admin/laporan/permintaan/TableLaporanPermintaanReagen.tsx
+++ src/components/percepat/admin/laporan/permintaan/TableLaporanPermintaanReagen.tsx
@@ -68,6 +68,8 @@ export default function TableLaporanPermintaanReagen({ url, limit, title, isWith
     ]
 
     const yearOptions = [
+        { value: '2025', label: '2025' },
+        { value: '2024', label: '2024' },
         { value: '2023', label: '2023' },
         { value: '2022', label: '2022' },
     ]
