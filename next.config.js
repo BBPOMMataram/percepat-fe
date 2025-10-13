d335d705e8c7e4f47d60561fc96ef647e7c825cc
arfani <arfanihidayat@gmail.com>
2025-10-13 11:49:30 +0800

    fix foto profil tidak tampil

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'media.giphy.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'auth.bbpommataram.com',
+            },
         ],
     }
 }
