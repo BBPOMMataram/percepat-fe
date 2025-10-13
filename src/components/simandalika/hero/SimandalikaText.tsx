81fb1ac65532e1d39234d8b9c4bd14947e2ed901
arfani <arfanihidayat@gmail.com>
2025-10-13 11:41:28 +0800

    update samesite to none untuk fix cookie tidak bisa muncul di production

--- src/components/simandalika/hero/SimandalikaText.tsx
+++ src/components/simandalika/hero/SimandalikaText.tsx
@@ -1,74 +1,77 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const SimandalikaText = () => {
     return (
         <StyledWrapper>
-            <button>
-                <span className='uppercase'>Si Mandalika</span>
-            </button>
+            <a href="#" className="btn-shine">SI MANDALIKA</a>
         </StyledWrapper>
     );
 }
 
 const StyledWrapper = styled.div`
-  button {
-   position: relative;
-   height: 50px;
-   padding: 0 30px;
-   border: 2px solid #e74d3c;
-   background: #fff;
-   user-select: none;
-   white-space: nowrap;
-   transition: all .05s linear;
-   font-family: inherit;
+  .btn-shine {
+    // position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 12px 48px;
+    color: #fff;
+    background: linear-gradient(to right, #9f9f9f 0, #fff 10%, #868686 20%);
+    background-position: 0;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: shine 5s infinite linear;
+    animation-fill-mode: forwards;
+    -webkit-text-size-adjust: none;
+    font-weight: 600;
+    font-size: 46px;
+    text-decoration: none;
+    white-space: nowrap;
+    font-family: sans-serif;
   }
-
-  button:before, button:after {
-   content: "";
-   position: absolute;
-   background: #fff;
-   transition: all .2s linear;
+  @-moz-keyframes shine {
+    0% {
+      background-position: 0;
+    }
+    60% {
+      background-position: 395px;
+    }
+    100% {
+      background-position: 395px;
+    }
   }
-
-  button:before {
-   width: calc(100% + 6px);
-   height: calc(100% - 16px);
-   top: 8px;
-   left: -3px;
+  @-webkit-keyframes shine {
+    0% {
+      background-position: 0;
+    }
+    60% {
+      background-position: 395px;
+    }
+    100% {
+      background-position: 395px;
+    }
   }
-
-  button:after {
-   width: calc(100% - 16px);
-   height: calc(100% + 6px);
-   top: -3px;
-   left: 8px;
+  @-o-keyframes shine {
+    0% {
+      background-position: 0;
+    }
+    60% {
+      background-position: 395px;
+    }
+    100% {
+      background-position: 395px;
+    }
   }
-
-  button:hover {
-   cursor: crosshair;
-  }
-
-  button:active {
-   transform: scale(0.95);
-  }
-
-  button:hover:before {
-   height: calc(100% - 32px);
-   top: 16px;
-  }
-
-  button:hover:after {
-   width: calc(100% - 32px);
-   left: 16px;
-  }
-
-  button span {
-   font-size: 25px;
-   z-index: 3;
-   position: relative;
-   font-weight: 600;
-   color: #e74d3c;
+  @keyframes shine {
+    0% {
+      background-position: 0;
+    }
+    60% {
+      background-position: 395px;
+    }
+    100% {
+      background-position: 395px;
+    }
   }`;
 
 export default SimandalikaText;
