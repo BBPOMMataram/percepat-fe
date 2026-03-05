f87a830ec5a298aa29f0bd92d6eb71efce0504bf
arfani <arfanihidayat@gmail.com>
2026-03-05 11:26:49 +0800

    use ecosystem.config.js & update deploy script

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+        {
+            name: "percepat",
+            script: "node_modules/next/dist/bin/next",
+            args: "start -p 3002",
+            cwd: "/home/bbpommataram.id/PERCEPAT",
+            instances: 1,
+            exec_mode: "fork",
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "500M",
+            env: {
+                NODE_ENV: "production"
+            }
+        }
+    ]
+};
