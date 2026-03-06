d8e6a25535c59618c3ff73936b767f93d067dcf1
arfani <arfanihidayat@gmail.com>
2026-03-06 11:06:00 +0800

    percepat - tambah api perlengkapan untuk lihat sisa stok oleh user

--- src/components/percepat-new/inventory/index.tsx
+++ src/components/percepat-new/inventory/index.tsx
@@ -1,10 +1,13 @@
 import cs from "classnames";
 import { useState } from "react";
 import TableAtk from "./TableAtk";
+import TablePerlengkapan from "./TablePerlengkapan";
 import TableReagen from "./TableReagen";
 
+type TypeBarang = 'reagen' | 'atk' | 'perlengkapan'
+
 const Inventory = () => {
-    const [isReagen, setIsReagen] = useState(true)
+    const [typeBarang, setTypeBarang] = useState<TypeBarang>('reagen')
 
     return (
         <>
@@ -12,34 +15,45 @@ const Inventory = () => {
                 <h2 className="text-2xl">Daftar Inventaris<span className="block sm:inline"> BBPOM di Mataram</span></h2>
                 <nav className="my-4">
                     <ul className="[&>li]:inline">
-                        <li onClick={() => setIsReagen(true)}>
+                        <li onClick={() => setTypeBarang('reagen')}>
                             <a className={cs("rounded py-1 inline-block w-20 mr-2", {
-                                "bg-quaternary": isReagen,
-                                "bg-gray-100 text-gray-300": !isReagen
+                                "bg-quaternary": typeBarang === 'reagen',
+                                "bg-gray-100 text-gray-300": typeBarang !== 'reagen'
                             })}
                             >Reagen</a>
                         </li>
-                        <li onClick={() => setIsReagen(false)}>
-                            <a className={cs("rounded py-1 inline-block w-20", {
-                                "bg-quaternary": !isReagen,
-                                "bg-gray-100 text-gray-300": isReagen
+                        <li onClick={() => setTypeBarang('atk')}>
+                            <a className={cs("rounded py-1 inline-block w-20 mr-2", {
+                                "bg-quaternary": typeBarang === 'atk',
+                                "bg-gray-100 text-gray-300": typeBarang !== 'atk'
                             })}>ATK</a>
+                        </li>
+                        <li onClick={() => setTypeBarang('perlengkapan')}>
+                            <a className={cs("rounded py-1 inline-block px-2", {
+                                "bg-quaternary": typeBarang === 'perlengkapan',
+                                "bg-gray-100 text-gray-300": typeBarang !== 'perlengkapan'
+                            })}>Perlengkapan</a>
                         </li>
                     </ul>
                 </nav>
             </section>
             <section className="content">
                 <div className="w-full">
-                    {isReagen ?
+                    {typeBarang === 'reagen' ?
                         <TableReagen
                             url='api/v1/barang/reagen'
                             title='Data Reagen'
                         />
-                        :
-                        <TableAtk
-                            url='api/v1/barang/atk'
-                            title='Data ATK'
-                        />
+                        : typeBarang === 'atk' ?
+                            <TableAtk
+                                url='api/v1/barang/atk'
+                                title='Data ATK'
+                            />
+                            :
+                            <TablePerlengkapan
+                                url='api/v1/barang/perlengkapan'
+                                title='Data Perlengkapan'
+                            />
                     }
                 </div>
             </section>
