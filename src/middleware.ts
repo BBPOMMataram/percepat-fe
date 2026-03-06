1eda6a50901cc4bebefb8365761f5f2024ea8ee5
arfani <arfanihidayat@gmail.com>
2026-03-06 09:43:15 +0800

    test cek cookie in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,6 +22,7 @@ export async function middleware(req: NextRequest) {
 
   if (!isProtected) return NextResponse.next();
 
+  console.log("COOKIE:", req.cookies.get("access_token"))
   if (!token) {
     const loginUrl = new URL("/login", req.url);
     loginUrl.searchParams.set("redirectUrl", pathname + search);
